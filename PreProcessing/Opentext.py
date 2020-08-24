1a412a5a67ce89aaba5e8bdf172a79685a4ef583
KYEONGCHAN LEE <cardchan@naver.com>
2020-08-24 16:06:36 +0900

    1. google까지 전처리 완료 2. Opentext에 Google STT 불러오기 추가(연채킴이 했던거) 3. Dictionary에 * : 곱하기 추가

--- PreProcessing/Opentext.py
+++ PreProcessing/Opentext.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[5]:
 
 
 def get_EBS_entered(s, e):
@@ -22,20 +22,20 @@ def get_EBS_entered(s, e):
                 if EBS_filenames.index(_) == 0:
                     EBS_integrated += f.read() 
                 else:
-                    EBS_integrated += '\n'+f.read()
+                    EBS_integrated += ' '+f.read()
     except:
         for _ in EBS_filenames:
             with open('data/' + _, 'r') as f:
                 if EBS_filenames.index(_) == 0:
                     EBS_integrated += f.read() 
                 else:
-                    EBS_integrated += '\n'+f.read()
+                    EBS_integrated += ' '+f.read()
     ####################################################
     
     return EBS_integrated
 
 
-# In[2]:
+# In[6]:
 
 
 def get_STT1_Google_entered(s, e):
@@ -57,20 +57,20 @@ def get_STT1_Google_entered(s, e):
                 if STT_filenames.index(_) == 0:
                     STT_integrated += f.read() 
                 else:
-                    STT_integrated += '\n'+f.read()
+                    STT_integrated += ' '+f.read()
     except:
         for _ in STT_filenames:
             with open('data/' + _, 'r') as f:
                 if STT_filenames.index(_) == 0:
                     STT_integrated += f.read() 
                 else:
-                    STT_integrated += '\n'+f.read()   
+                    STT_integrated += ' '+f.read()   
     ####################################################
     
     return STT_integrated
 
 
-# In[3]:
+# In[7]:
 
 
 def get_STT1_Transcribe_entered(s, e):
@@ -92,20 +92,20 @@ def get_STT1_Transcribe_entered(s, e):
                 if STT_filenames.index(_) == 0:
                     STT_integrated += f.read() 
                 else:
-                    STT_integrated += '\n'+f.read()
+                    STT_integrated += ' '+f.read()
     except:
         for _ in STT_filenames:
             with open('data/' + _, 'r') as f:
                 if STT_filenames.index(_) == 0:
                     STT_integrated += f.read() 
                 else:
-                    STT_integrated += '\n'+f.read()
+                    STT_integrated += ' '+f.read()
     ####################################################
     
     return STT_integrated
 
 
-# In[4]:
+# In[1]:
 
 
 def get_EBS(s, e):
@@ -126,17 +126,52 @@ def get_EBS(s, e):
                 if EBS_filenames.index(_) == 0:
                     EBS_integrated += f.read() 
                 else:
-                    EBS_integrated += '\n'+f.read()
+                    EBS_integrated += ' '+f.read()
     except:
         for _ in EBS_filenames:
             with open('data/' + _, 'r') as f:
                 if EBS_filenames.index(_) == 0:
                     EBS_integrated += f.read() 
                 else:
-                    EBS_integrated += '\n'+f.read()
+                    EBS_integrated += ' '+f.read()
     ####################################################
     
     return EBS_integrated
+
+
+# In[1]:
+
+
+def get_STT1_Google(s, e):
+    # 파일 이름 만들기 #################################################
+    STT_filenames = []
+    for i in range(s, e+1):
+        n = '{0:05d}'.format(i)
+        STT_fname = 'lec' + n + '_STT1_Google.txt'
+        STT_filenames.append(STT_fname)
+    print(STT_filenames)
+    ####################################################
+    
+    # 파일 불러오기 ###################################################
+    STT_integrated = ''
+    
+    try:
+        for _ in STT_filenames:
+            with open('data/' + _, 'r', encoding = 'euc-kr') as f:
+                if STT_filenames.index(_) == 0:
+                    STT_integrated += f.read() 
+                else:
+                    STT_integrated += '\n'+f.read()
+    except:
+        for _ in STT_filenames:
+            with open('data/' + _, 'r') as f:
+                if STT_filenames.index(_) == 0:
+                    STT_integrated += f.read() 
+                else:
+                    STT_integrated += '\n'+f.read()   
+    ####################################################
+    
+    return STT_integrated
 
 
 # In[ ]:
