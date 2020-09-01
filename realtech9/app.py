26491ad58c3215e963d471f167946be66567d7bb
KYEONGCHAN LEE <cardchan@naver.com>
2020-09-01 13:22:04 +0900

    임시

--- realtech9/app.py
+++ realtech9/app.py
@@ -1,19 +1,31 @@
 import upload as upload
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_dropzone import Dropzone
+from flask_admin.contrib.fileadmin import FileAdmin
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin
+from flask_sqlalchemy import SQLAlchemy
+from flask_dropzone import Dropzone
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 upload_dir = os.path.join(basedir, 'uploads')
 
+####################################################
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
-db = SQLAlchemy(app)
-admin = Admin(app, name = 'Test')
-admin.add_view(ModelView(User, db.session))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite" #여기만 바꿔주면 다양한 db와 연동가능
+app.config['SECRET_KEY'] = 'sun'
+app.config['UPLOADED_PATH'] = upload_dir
+# app.config['FLASK_ADMIN_SWATCH'] = 'cerulean' #admin 테마
+####################################################
+
+####################################################
+db = SQLAlchemy(app) #객체와 db를 연동시킨다. sql처럼 똑같이 사용할 수 있다.
+# SQLAlchemy 기능을 갖다쓸 수 있다. 위 아니면 아래 중 하나 쓰면됨. init를 선호.
+# db.init_app((app)
+admin = Admin(name='test')
+admin.init_app(app)
+dropzone = Dropzone(app) #
+####################################################
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,8 +33,11 @@ class User(db.Model):
     email = db.Column(db.String, unique=True, nullable=False)
 
 
-#app.config['SECRET_KEY'] = 'bana'
-app.config['UPLOADED_PATH'] = upload_dir
+admin.add_view(ModelView(User, db.session))
+admin.add_view(FileAdmin(upload_dir, name = 'MOON'))
+
+with open("uploads/lec00001_EBS.txt", 'r', encoding='utf-8') as f:
+    a = f.read()
 
 @app.route('/')
 def main():
@@ -40,7 +55,7 @@ def list():
 def upload():
     if request.method =='POST':
         f = request.files.get('file')
-        print(f.save(os.path.join(upload_dir, f.filename)))
+        f.save(os.path.join(upload_dir, f.filename))
 
     return render_template('stt.html')
 
@@ -48,17 +63,29 @@ def upload():
 def dand():
     return render_template('dd.html')
 
-@app.route('/convert/')
-def convert():
-    return render_template('RNN.py')
+@app.route("/forward/", methods=['POST'])
+def move_forward():
+    #Moving forward code
+    forward_message = "Moving Forward..."
+    return render_template('index.html', forward_message=a);
 
-@app.route('/list')
-def list():
-    for i in User.query.all():
-        print(i)
-        print(i.username)
-        print(i.email)
-    print(User.query.get(1))
+
+
+def pnt(a):
+    return a[:30]
+
+
+# @app.route('/convert')
+# def convert():
+#     return render_template('RNN.py')
+
+# @app.route('/list')
+# def list():
+#     for i in User.query.all():
+#         print(i)
+#         print(i.username)
+#         print(i.email)
+#     print(User.query.get(1))
 
 
 if __name__ == '__main__':
--- realtech9/test.py
+++ realtech9/test.py
@@ -1,9 +1,15 @@
 import os, sys
 
-uppath = lambda _path, n: os.sep.join(_path.split(os.sep)[:-n])
-github_base_dir = uppath(__file__, 2)
-print(github_base_dir)
-sys.path.append(os.path.join(github_base_dir, 'PreProcessing'))
+# uppath = lambda _path, n: os.sep.join(_path.split(os.sep)[:-n])
+# github_base_dir = uppath(__file__, 2)
+# print(github_base_dir)
+# sys.path.append(os.path.join(github_base_dir, 'PreProcessing'))
+# 
 
-from Dictionary import kopro, sd
-print(sd)
+# print(os.getcwd())
+with open("uploads/lec00001_EBS.txt", 'r', encoding='utf-8') as f:
+    a = f.read()
+print(a)
+
+# from Dictionary import sd
+# print(sd)
